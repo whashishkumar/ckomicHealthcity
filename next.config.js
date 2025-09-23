0be0cc09593fe5a6e2a1c8c8066f6efdc459e58f
whashishkumar <wh.ashishkumar@gmail.com>
2025-09-23 09:52:24 +0530

    fix image optimazation

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "ckosmic.whdev.in",
         port: "",
-        pathname: "/storage/**", // allow all images under /storage
+        pathname: "/**", // allow all images under /storage
       },
     ],
-    unoptimized: process.env.NEXT_PUBLIC_IMAGE_URL === "false",
+    unoptimized: false,
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
